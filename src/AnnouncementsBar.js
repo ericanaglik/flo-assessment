aff8095d6f7b2ac5169a1c40ff8502d07c2f37ae
erica2605@live.com <erica2605@live.com>
2021-01-12 17:24:41 -0500

    Created announcements bar component

---
+++ src/AnnouncementsBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const AnnouncementsBar = props => {
+
+  return (
+    <div>
+      <a href="url">COVID-19 Response</a>
+      |
+      <a href="url">The Mask Project</a>
+      <a href="url">Refer &amp; Get 15%</a>
+    </div>
+  );
+};
+
+export default AnnouncementsBar;
+
+
