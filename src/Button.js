92987b889aaf65c99176c95070100200f2c8df74
erica2605@live.com <erica2605@live.com>
2021-01-13 15:30:03 -0500

    Created button component and styled it

---
+++ src/Button.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Button.scss'
+
+const Button = props => {
+  const {
+    buttonText
+  } = props
+  return (
+    <div>
+      <button>{buttonText}</button>
+    </div>
+  )
+}
+
+export default Button
