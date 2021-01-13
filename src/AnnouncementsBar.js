15e66197177d6576995a75f669ea65e3aa7b0790
erica2605@live.com <erica2605@live.com>
2021-01-12 19:04:46 -0500

    Styled Announcements Bar

--- src/AnnouncementsBar.js
+++ src/AnnouncementsBar.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import './AnnouncementsBar.scss'
 
 const AnnouncementsBar = props => {
 
   return (
-    <div>
-      <a href="url">COVID-19 Response</a>
-      |
-      <a href="url">The Mask Project</a>
-      <a href="url">Refer &amp; Get 15%</a>
+    <div className="announcements-bar">
+      <div className="announcements-container">
+        <a href="url">COVID-19 Response</a>
+        |
+        <a href="url">The Mask Project</a>
+      </div>
+      <div className="referral-container">
+        <a href="url">Refer &amp; Get 15%</a>
+      </div>
     </div>
   );
 };
