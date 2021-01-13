cd3a4192804434f5296ce12889811e0013defa56
erica2605@live.com <erica2605@live.com>
2021-01-13 17:56:24 -0500

    How Our Rugs Work section styled and scaling dynamically

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.scss';
 import NavBar from './NavBar'
+import Button from './Button'
 
 
 function App() {
@@ -10,9 +11,17 @@ function App() {
       <div className="landing-container">
         <div className="header">
           <h1>The Perfect Rug for Dog Owners</h1>
+          <Button buttonText={"Shop Dog-Friendly Rugs"}/>
+        </div>
+        <div className="how-it-works-section">
+          <img src="https://cdn.shopify.com/s/files/1/0284/2666/6115/files/Peelup_HowItWorks.jpg?v=1579042343"></img>
+          <div className="text-container">
+            <h2>How Our Rugs Work</h2>
+            <p>Ruggable is a 2-piece system that features a lightweight Rug Cover and a non-slip Rug Pad. All our Covers are water resistant, stain-resistant and can be detached and reattached to its Pad without losing its grip, so you can put them in the wash as often as you need.</p>
+            <Button buttonText={"Shop Best Sellers"}/>
+          </div>
         </div>
       </div>
-
     </div>
   );
 }
