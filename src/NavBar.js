b71e789ce4f8e1a496dca6a10a9b093dd083b129
erica2605@live.com <erica2605@live.com>
2021-01-13 14:03:34 -0500

    Navbar bottom border now extends width of page

--- src/NavBar.js
+++ src/NavBar.js
@@ -9,31 +9,33 @@ const NavBar = props => {
   return (
     <div>
       <AnnouncementsBar/>
-      <div className="navbar">
-        <div className="logo">
-          <img  
-            height="24px"
-            width="230px"
-            src="https://images.ctfassets.net/9y4fi2gkk4ov/1UhZTIVtnqHRkeYiKopBGx/2062ac70d56d88b8dba29aff25107782/Ruggable_Logotype_Black.svg?w=300&fit=scale" 
-            alt="Ruggable Logo"
-          ></img>
-        </div>
-        <div className="nav-links">
-          
-          <div className="link-container"><a href="url">Size</a></div>
-          <div className="link-container"><a href="url">Colors</a></div>
-          <div className="link-container"><a href="url">Style</a></div>
-          <div className="link-container"><a href="url">More</a></div>
-          <div className="link-container"><a href="url">How It Works</a></div>
-        </div>
-        <div className="search">
-          <form>
-            <input type="search" placeholder="What are you looking for?"></input>
-            <FontAwesomeIcon className="search-icon" icon={faSearch}/>
-          </form>  
-        </div>
-        <div className="cart">
-          <FontAwesomeIcon icon={faShoppingCart}/>
+      <div className="nav-wrapper">
+        <div className="navbar">
+          <div className="logo">
+            <img  
+              height="24px"
+              width="230px"
+              src="https://images.ctfassets.net/9y4fi2gkk4ov/1UhZTIVtnqHRkeYiKopBGx/2062ac70d56d88b8dba29aff25107782/Ruggable_Logotype_Black.svg?w=300&fit=scale" 
+              alt="Ruggable Logo"
+            ></img>
+          </div>
+          <div className="nav-links">
+            
+            <div className="link-container"><a href="url">Size</a></div>
+            <div className="link-container"><a href="url">Colors</a></div>
+            <div className="link-container"><a href="url">Style</a></div>
+            <div className="link-container"><a href="url">More</a></div>
+            <div className="link-container"><a href="url">How It Works</a></div>
+          </div>
+          <div className="search">
+            <form>
+              <input type="search" placeholder="What are you looking for?"></input>
+              <FontAwesomeIcon className="search-icon" icon={faSearch}/>
+            </form>  
+          </div>
+          <div className="cart">
+            <FontAwesomeIcon icon={faShoppingCart}/>
+          </div>
         </div>
       </div>
     </div>
