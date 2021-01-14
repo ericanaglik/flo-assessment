7a9100687a9f8ff52fff43583d119b48ecbd7ac0
erica2605@live.com <erica2605@live.com>
2021-01-13 19:53:37 -0500

    Added IconCard component as a child of CardSection component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 import NavBar from './NavBar'
 import Button from './Button'
 import CardSection from './CardSection'
+import IconCard from './IconCard'
 
 
 function App() {
@@ -25,7 +26,23 @@ function App() {
         <CardSection 
           titleText={"Functional and Dog-Approved!"}
           buttonText={"Shop Dog-Friendly Rugs"}
-        />
+        >
+          <IconCard
+            iconURL={"https://cdn.shopify.com/s/files/1/0284/2666/6115/files/Ruggable-Icon-ValueProp-Washable.svg?4206"}
+            titleText={"Machine Washable"}
+            descriptionText={"Meet the first rug made for your washing machine. With our washable Rug Covers, you’ll never have to throw away a dirty rug again!"}
+          />
+          <IconCard
+            iconURL={"https://cdn.shopify.com/s/files/1/0284/2666/6115/files/Icon_PetFriendly.svg?4206"}
+            titleText={"Stain Resistant"}
+            descriptionText={"Ideal for high traffic homes where life and inevitable messes happen. You can wash the Cover and spot clean the Pad to remove stains and smells for a fresh, odor-free rug."}
+          />
+          <IconCard
+            iconURL={"https://cdn.shopify.com/s/files/1/0284/2666/6115/files/Icon_Waterproof.svg?4206"}
+            titleText={"Water Resistant"}
+            descriptionText={"We get it, accidents happen. That’s why our rugs have a built-in water resistant layer that keeps liquid from seeping through and dripping onto the floor."}
+          />
+        </CardSection>
         <CardSection 
           titleText={"Cleanup Made Easy"}
           buttonText={"Shop Dog-Friendly Rugs"}
