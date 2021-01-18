f07c42eeb91b82148ff6023aef3c48d2492a35fe
erica2605@live.com <erica2605@live.com>
2021-01-18 18:20:23 -0500

    Attempt 2 at fixing mobile scaling problems

--- src/NavBar.js
+++ src/NavBar.js
@@ -24,6 +24,7 @@ const NavBar = props => {
               width="230px"
               src="https://images.ctfassets.net/9y4fi2gkk4ov/1UhZTIVtnqHRkeYiKopBGx/2062ac70d56d88b8dba29aff25107782/Ruggable_Logotype_Black.svg?w=300&fit=scale" 
               alt="Ruggable Logo"
+              className="ruggable-logo"
             ></img>
           </div>
           <div className="nav-links">
@@ -32,7 +33,7 @@ const NavBar = props => {
             <div className="link-container"><a href="url">Colors</a><div className="hover-bottom"></div></div>
             <div className="link-container"><a href="url">Style</a><div className="hover-bottom"></div></div>
             <div className="link-container"><a href="url">More</a><div className="hover-bottom"></div></div>
-            <div className="link-container"><a href="url">How It Works</a><div className="hover-bottom"></div></div>
+            <div className="link-container"><a href="url">How It Works</a></div>
           </div>
           <div className="search">
             <form>
